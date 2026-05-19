308eb9b3457188927e3727cfe8e69528e8abe496
abiiiaja <kalindraaraya@gmail.com>
2026-05-19 20:04:54 +0700

    deklarasi member dari class remote lampu

--- enkapsulasi.cpp
+++ enkapsulasi.cpp
@@ -2,5 +2,13 @@
 using namespace std;
 
 class remotelampu {
-
+private:
+    string saklarNo[10];
+public:
+    void setSaklarNo(int i, string value) {
+        saklarNo[i] = value;
+    }
+    string getSaklarNo(int i) {
+        return saklarNo[i];
+    }
 };
